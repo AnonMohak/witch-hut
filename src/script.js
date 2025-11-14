4b2f8ec2821f59c707783e31b74e6a33bd382edf
mohak <notmohakmohan@gmail.com>
2025-11-14 22:18:46 +0530

    remove useless

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { Timer } from 'three/addons/misc/Timer.js'
 import GUI from 'lil-gui'
-import Stats from 'stats.js'
+//import Stats from 'stats.js'
 import {Sky} from 'three/addons/objects/Sky.js'
 
 /**
